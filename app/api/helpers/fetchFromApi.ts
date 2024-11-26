f686442d7c86b0124b57df7c4640f3c134e1b30b
lucasdellasala <lucasdellasala@gmail.com>
2024-11-26 18:28:29 -0300

    revalidate api fetch

--- app/api/helpers/fetchFromApi.ts
+++ app/api/helpers/fetchFromApi.ts
@@ -1,5 +1,5 @@
 export async function fetchFromApi<T>(endpoint: string): Promise<T> {
-  const response = await fetch(endpoint);
+  const response = await fetch(endpoint, { next: { revalidate: 60 }});
 
   if (!response.ok) {
     throw new Error(`Failed to fetch data from ${endpoint}`);
