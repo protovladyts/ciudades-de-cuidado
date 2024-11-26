ff961b264717e69450ea92a8b372484ebf2035ee
lucasdellasala <lucasdellasala@gmail.com>
2024-11-26 17:55:50 -0300

    fix footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,59 +1,17 @@
 "use client";
 
 import React from "react";
-import { FaXTwitter, FaLink, FaInstagram, FaWhatsapp } from "react-icons/fa6";
 
 import Image from "next/image";
 import { useLanguage } from "../LanguageContext";
 import { mapLayoutData } from "../api/wordpress/mappers/mapLayoutData";
-import { SocialMediaLinks, WordPressLayout } from "../api/wordpress/types/WordPressLayout";
+import { WordPressLayout } from "../api/wordpress/types/WordPressLayout";
 import { Paragraph } from "./Paragraph";
 
 // Separadores y componentes estilizados
 const GradientSeparator = () => (
   <div className="w-full h-0.5 bg-gradient-to-r from-[#E551C7] via-[#D47C45] via-[#E5D548] to-[#83F42F] mt-0 md:mt-12 lg:mt-6" />
 );
-
-const CircleLink = ({
-  href,
-  label,
-  children,
-}: {
-  href: string;
-  label: string;
-  children: React.ReactNode;
-}) => (
-  <a
-    href={href}
-    aria-label={label}
-    className="border-[2.5px] 3xl:border-[3.5px] rounded-full border-[#383D38]  text-[#383D38] hover:text-[#5a5c5a] min-[360]:border-[2.5px] min-[360]:rounded-full p-1 min-[360]:border-[#383D38] hover:border-[#5a5c5a] transition-colors"
-  >
-    {children}
-  </a>
-);
-
-const ContactLinks = ({ className, socialMedia }: { className?: string, socialMedia: SocialMediaLinks }) => {
-  const links = [
-    { href: socialMedia.instagram_link, label: "Instagram", icon: <FaInstagram /> },
-    { href: socialMedia.whatsapp_link, label: "WhatsApp", icon: <FaWhatsapp /> },
-    { href: socialMedia.twitter_link, label: "Twitter", icon: <FaXTwitter /> },
-    { href: socialMedia.custom_link, label: "Website", icon: <FaLink /> },
-  ];
-
-  return (
-    <div className={`grid grid-cols-4 gap-4 mb-4 ${className}`}>
-      {links.map(({ href, label, icon }) => (
-        <CircleLink key={label} href={href} label={label}>
-          <span className="sr-only">{label}</span>
-          {React.cloneElement(icon, {
-            className:
-              "h-10 w-10 3xl:h-12 3xl:w-12 md:h-8 md:w-8 4xs:h-8 4xs:w-8",
-          })}
-        </CircleLink>
-      ))}
-    </div>
-  );
-};
 
 // Footer principal
 export function Footer({ content }: { content: WordPressLayout }) {
@@ -109,7 +67,6 @@ export function Footer({ content }: { content: WordPressLayout }) {
     </>
   );
 
-
   return (
     <footer className="relative py-8 px-4 sm:px-6 lg:px-8">
       {/* Imágenes de fondo */}
@@ -118,7 +75,7 @@ export function Footer({ content }: { content: WordPressLayout }) {
       <div className="relative z-10 max-w-7xl mx-auto flex flex-col md:flex-row">
         <div className="w-full md:w-1/2 lg:w-3/4 xl:w-3/5 3xl:w-3/4 mb-8 md:mb-0 md:pr-8 lg:ml-16 xl:ml-52 2xl:ml-36 3xl:ml-0">
           <div className="w-full grid grid-cols-12 place-items-start">
-            <ContactLinks className="col-span-10" socialMedia={localizedFooter.social_media} />
+            <Image className="w-auto col-span-10 mt-2 mx-0 3xs:h-16 4xs:h-10 2xl:h-20 3xl:h-20 4xl:h-24" src="https://i.ibb.co/Pw54pkm/LOGO-FRL-02.png" alt="fundation logo" width={100} height={100}/>
           </div>
 
           <GradientSeparator />
